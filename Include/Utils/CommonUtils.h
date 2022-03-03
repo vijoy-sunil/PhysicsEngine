7da0bb9ff18013d304b31858d6f584911431a40c
vijoy-sunil <vijoy.sunil@gmail.com>
2022-03-03 10:05:44 +0530

    vector definitions

--- Include/Utils/CommonUtils.h
+++ Include/Utils/CommonUtils.h
@@ -1,14 +1,17 @@
 #ifndef UTILS_COMMONUTILS_H
 #define UTILS_COMMONUTILS_H
 
-typedef struct{
+typedef struct vector2{
     float x;
     float y;
+}vector2f_t;
 
-    int getDirection(float attr){
-        return attr < 0 ? -1 : 1;
-    }
-}vector_t;
+typedef struct vector4{
+    vector2f_t v0;
+    vector2f_t v1;
+    vector2f_t v2;
+    vector2f_t v3;
+}vector4v_t;
 
 extern double xPos, yPos;
 extern volatile bool mouseClicked;
