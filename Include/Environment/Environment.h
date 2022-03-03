a986cf68cc8327fe69e9d69e1c40831a134a8f64
vijoy-sunil <vijoy.sunil@gmail.com>
2022-03-03 10:18:21 +0530

    mesh drawing walls and agents, collision detection on spawn

--- Include/Environment/Environment.h
+++ Include/Environment/Environment.h
@@ -1,22 +1,22 @@
 #ifndef ENVIRONMENT_H
 #define ENVIRONMENT_H
 
-#include "../../Include/Utils/GridUtils.h"
 #include "../../Include/Agent/Agent.h"
+#include "../../Include/Utils/GridUtils.h"
 #include "../../Include/Utils/CommonUtils.h"
 #include <vector>
 
 /* attributes of the environment core
- * dt                   : simulation timestep
+ * dt                   : simulation timestep (s)
  * gravity              : gravitational force on the atmosphere operates uniformly (m/s2)
  * density              : drag experienced by a falling object depends on the air density (kg/m3)
- * surfaceSmoothness    : surface smoothness for different types of surfaces
+ * walls                : 4 vertices of walls
 */
 typedef struct{
     float dt;
     float gravity;
     float density;
-    std::vector<float> surfaceSmoothness;
+    std::vector<vector4v_t> walls;
 }envAttribute_t;
 
 class EnvironmentClass: public AgentClass, public GridUtilsClass{
@@ -29,16 +29,18 @@ class EnvironmentClass: public AgentClass, public GridUtilsClass{
         std::vector<int> agentIDList;
 
     protected:
-        /* agent fns
+        /* agent draw fns
         */
+        void setMeshAsState(vector4v_t vertices, cellState_t state);
         bool spawnAgent(agentAttribute_t attr);
+        bool killAgent(agentAttribute_t attr);
         bool validPendingAgent(int agentID);
         /* physics
         */
-        std::pair<int, int> computeDrag(agentAttribute_t attr);
-        std::pair<int, int> computeNetForce(agentAttribute_t attr);
+        vector2f_t computeDragForce(agentAttribute_t attr);
+        vector2f_t computeNormalForce(agentAttribute_t attr);
+        vector2f_t computeNetForce(agentAttribute_t attr);
         std::pair<int, int> computeMotion(agentAttribute_t attr);
-        bool addForce(std::pair<int, int> pos, vector_t force);
 
     public:
         EnvironmentClass(float _dt, float _g, float _d, int _N, int _scale, bool noStroke);
--- Source/Environment/Environment.cpp
+++ Source/Environment/Environment.cpp
@@ -1,7 +1,7 @@
 #include "../../Include/Environment/Environment.h"
 #include "../../Include/Utils/Constants.h"
-#include "../../Include/Utils/CommonUtils.h"
 #include <iostream>
+#include <algorithm>
 
 EnvironmentClass::EnvironmentClass(float _dt, float _g, float _d, 
                                    int _N, int _scale, bool noStroke):
@@ -18,82 +18,141 @@ GridUtilsClass(_N, _scale, noStroke)
 EnvironmentClass::~EnvironmentClass(void){
 }
 
+void EnvironmentClass::setMeshAsState(vector4v_t vertices, cellState_t state){
+    setCellStreamAsState({vertices.v0.x, vertices.v0.y}, 
+                         {vertices.v1.x, vertices.v1.y}, state);
+    setCellStreamAsState({vertices.v1.x, vertices.v1.y}, 
+                         {vertices.v2.x, vertices.v2.y}, state);
+    setCellStreamAsState({vertices.v2.x, vertices.v2.y}, 
+                         {vertices.v3.x, vertices.v3.y}, state);
+    setCellStreamAsState({vertices.v3.x, vertices.v3.y}, 
+                         {vertices.v0.x, vertices.v0.y}, state);
+}
+
 bool EnvironmentClass::spawnAgent(agentAttribute_t attr){
-    /* draw agent cell/block
+    /* draw agent
     */
-    int numCells = attr.numParticles;
-    if(attr.shape == RECTANGLE)
-        numCells == 1 ? setCellAsAgent(attr.com) : setBlockAsAgent(attr.com, attr.dim);
+    setMeshAsState(attr.vertices, AGENT);
+    /* draw com
+    */
+    setCellAsAgent({attr.positionCenterOfMass.x, attr.positionCenterOfMass.y});
+    return true;
+}
+
+bool EnvironmentClass::killAgent(agentAttribute_t attr){
+    /* clear agent
+    */
+    setMeshAsState(attr.vertices, FREE);
+    /* clear com
+    */
+    setCellAsFree({attr.positionCenterOfMass.x, attr.positionCenterOfMass.y});
     return true;
 }
 
 bool EnvironmentClass::validPendingAgent(int agentID){
-    agentAttribute_t attr = agentMap[agentID];
-    /* check if com is in free cell
-    */
-    if(!isCellFree(attr.com))
-        return false;
-    /* check all particles
-    */
-    if(attr.numParticles != 1){
-        std::pair<int, int> dimFmt = {attr.dim.first/2, attr.dim.second/2};
-        int r, c;
-        int i = attr.com.first;
-        int j = attr.com.second;
+    vector4v_t agentVertices = agentMap[agentID].vertices;
+    float agentX1 = std::min(agentVertices.v0.x, 
+                    std::min(agentVertices.v1.x, 
+                    std::min(agentVertices.v2.x, agentVertices.v3.x)));
 
-        for(r = -dimFmt.first; r <= dimFmt.first; r++){
-            for(c = -dimFmt.second; c <= dimFmt.second; c++){
-                if(!isCellFree({i + r, j + c}))
-                    return false;
-            }
-        }
+    float agentX2 = std::max(agentVertices.v0.x, 
+                    std::max(agentVertices.v1.x, 
+                    std::max(agentVertices.v2.x, agentVertices.v3.x)));
+
+    float agentY1 = std::min(agentVertices.v0.y, 
+                    std::min(agentVertices.v1.y, 
+                    std::min(agentVertices.v2.y, agentVertices.v3.y)));
+
+    float agentY2 = std::max(agentVertices.v0.y, 
+                    std::max(agentVertices.v1.y, 
+                    std::max(agentVertices.v2.y, agentVertices.v3.y)));
+
+    /* check for collision with  walls before spawning in
+    */
+    for(int i = 0; i < envAttribute.walls.size(); i++){
+        vector4v_t wallVertices = envAttribute.walls[i];
+        float wallX1 = wallVertices.v0.x;
+        float wallX2 = wallVertices.v1.x;
+        float wallY1 = wallVertices.v0.y;
+        float wallY2 = wallVertices.v3.y;
+
+        /* check inerval overlap
+        */
+        if(((wallX1 <= agentX2) && (agentX1 <= wallX2)) &&
+           ((wallY1 <= agentY2) && (agentY1 <= wallY2)))
+            return false;
     }
+    /* TODO : check spawn agent overlap with other agent
+    */
+
     return true;
 }
 
-std::pair<int, int> EnvironmentClass::computeForce(agentAttribute_t attr){
-    /* gravitational force
-    */
-    std::pair<float, float> gravitationalForce = {0, attr.mass * envAttribute.gravity};
-
-    /* sum up all forces acting on the agent
-    */
-    std::pair<float, float> cumulativeForce = {0.0, 0.0};
-    cumulativeForce.first += gravitationalForce.first;
-    cumulativeForce.second += gravitationalForce.second;
-
-    return cumulativeForce;
-}
-
-std::pair<int, int> EnvironmentClass::computeMotion(agentAttribute_t attr){
-    int posX = attr.com.first;
-    int posY = attr.com.second;
-
-    posX += attr.velocity.first * envAttribute.dt;
-    posY += attr.velocity.second * envAttribute.dt;
-
-    return {posX, posY};
-}
-
-/* |-----------------------------------------------------|
- * |         OVERRIDE BASE CLASS FUNCTIONS               |
- * |-----------------------------------------------------|
-*/
 void EnvironmentClass::setInitialCells(void){
 #if ENV_SCENE_0 == 1
-    int borderWidth = 0.03 * N;
+    float borderWidth = 0.03 * N;
+    /* consider walls as static agents, we use the attributes to calculate bounding vertices
+    */ 
+    agentAttribute_t attr;
+    /* wall #1
+    */
+    attr.width = N - 1;
+    attr.height = borderWidth;
+    attr.orientation = 0.0;
+    attr.positionCenterOfMass = {attr.width/2, attr.height/2};
+    attr.vertices = computeBoundingVertices(attr);
+    /* add vertices to env attribute
+    */
+    envAttribute.walls.push_back(attr.vertices);
+
+    /* wall #2
+    */
+    attr.width = N - 1;
+    attr.height = borderWidth;
+    attr.orientation = 0.0;
+    attr.positionCenterOfMass = {attr.width/2, N - 1 - attr.height/2};
+    attr.vertices = computeBoundingVertices(attr);
+    /* add vertices to env attribute
+    */
+    envAttribute.walls.push_back(attr.vertices);
+
+    /* wall #3
+    */
+    attr.width = borderWidth;
+    attr.height = (N - 1) - (2 * borderWidth);
+    attr.orientation = 0.0;
+    attr.positionCenterOfMass = {N - 1 - attr.width/2, (N - 1)/2.0f};
+    attr.vertices = computeBoundingVertices(attr);
+    /* add vertices to env attribute
+    */
+    envAttribute.walls.push_back(attr.vertices);  
+
+    /* wall #4
+    */
+    attr.width = borderWidth;
+    attr.height = (N - 1) - (2 * borderWidth);
+    attr.orientation = 0.0;
+    attr.positionCenterOfMass = {attr.width/2, (N - 1)/2.0f};
+    attr.vertices = computeBoundingVertices(attr);
+    /* add vertices to env attribute
+    */
+    envAttribute.walls.push_back(attr.vertices);  
+
+    /* wall #5
+    */
+    attr.width = (N - 1)/2 - borderWidth;
+    attr.height = borderWidth;
+    attr.orientation = 0.0;
+    attr.positionCenterOfMass = {((3 * (N - 1))/4.0f) - borderWidth/2, (N - 1)/2.0f};
+    attr.vertices = computeBoundingVertices(attr);
+    /* add vertices to env attribute
+    */
+    envAttribute.walls.push_back(attr.vertices); 
+
     /* surfaces
     */
-    setCellAsObstacleStream({0, 0}, {N-1, 0}, borderWidth, BOTTOM);
-    setCellAsObstacleStream({0, 0}, {0, N-1}, borderWidth, LEFT);
-    setCellAsObstacleStream({N-1, 0}, {N-1, N-1}, borderWidth, RIGHT);
-    setCellAsObstacleStream({N/2, N/2}, {N-1, N/2}, borderWidth, BOTTOM);
-    /* friction
-    */
-    envAttribute.surfaceSmoothness.push_back(0.5);
-    envAttribute.surfaceSmoothness.push_back(0.5);
-    envAttribute.surfaceSmoothness.push_back(0.5);
-    envAttribute.surfaceSmoothness.push_back(0.5);
+    for(int i = 0; i < envAttribute.walls.size(); i++)
+        setMeshAsState(envAttribute.walls[i], OBSTACLE);
 #endif
 }
 
@@ -103,57 +162,21 @@ void EnvironmentClass::simulationStep(void){
     for(int i = 0; i < agentIDList.size(); i++){
         int agentID = agentIDList[i];
         agentAttribute_t attr = agentMap[agentID];
-        /* |-----------------------------------------------------|
-         * |            PHYSICS COMPUTE FORCES                   |
-         * | calculate velocity and acceleration based on forces |
-         * |-----------------------------------------------------|
-         */
-        std::pair<float, float> force = computeForce(attr);
-        /* update acceleration and velocity
-        */
-        std::pair<float, float> acceleration = {force.first/attr.mass, force.second/attr.mass};
-        attr.velocity.first += acceleration.first * envAttribute.dt;
-        attr.velocity.second += acceleration.second * envAttribute.dt;
-        /* update map
-        */
-        agentMap[agentID] = attr;
-    }
 
-    /* update all agents' com, updated agents will be drawn in next frame. NOTE: save previous 
-     * agent.com before overwriting it. If they are the same, we don't need to clear the agent. 
-     * If different, clear previous.
-    */
-    for(int i = 0; i < agentIDList.size(); i++){
-        int agentID = agentIDList[i];
-        agentAttribute_t attr = agentMap[agentID];
-
-        std::pair<int, int> currCom = attr.com;
-        /* |-----------------------------------------------------|
-         * |            PHYSICS COMPUTE MOTION                   |
-         * |       calculate nextCom based on velocity           |
-         * |-----------------------------------------------------|
-         */
-        std::pair<int, int> nextCom = computeMotion(attr);
-        /* clear agent if currCom != nextCom
-        */
-        if(currCom.first != nextCom.first || currCom.second != nextCom.second){
-            if(attr.shape == RECTANGLE)
-                attr.numParticles == 1 ? setCellAsFree(attr.com) : 
-                setBlockAsFree(attr.com, attr.dim);      
-
-            /* overwrite currCom with nextCom and update agent map
-            */
-            attr.com = nextCom;   
-            agentMap[agentID] = attr;
-        }
     }
 
     /* create agent on mouse click
     */
     if(mouseClicked){
         mouseClicked = false;
-        std::pair<int, int> cellPos = mouseAction(xPos, yPos);
-        int agentID = createAgent_test(cellPos);
+        std::pair<float, float> cellPos = mouseAction(xPos, yPos);
+        int agentID = createAgent(1.0, 
+                                 101, 51, 
+                                 0.0, 
+                                 0.0,
+                                 0.0,
+                                 {cellPos.first, cellPos.second},
+                                 {0.0, 0.0});
         /* create agent only if mouse clicked in free cell; and if all particles are in free cell
         */
         if(validPendingAgent(agentID))
@@ -171,10 +194,7 @@ void EnvironmentClass::simulationStep(void){
     }
 
 #if DEBUG_PRINT == 1
-    if(agentIDList.size() != 0){
-        agentAttribute_t attr = agentMap[0];
-        std::cout<<"[DEBUG] agent vel: "<<attr.velocity.first<<","
-        <<attr.velocity.second<<std::endl;
-    }
+    if(agentIDList.size() != 0)
+        dumpAgentMap(0);
 #endif
 }
