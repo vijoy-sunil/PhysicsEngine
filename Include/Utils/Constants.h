cc8854832d7d19eb8acf378be69971c3396980ee
vijoy-sunil <vijoy.sunil@gmail.com>
2022-03-03 20:31:30 +0530

    removed debug macro

--- Include/Utils/Constants.h
+++ Include/Utils/Constants.h
@@ -15,10 +15,8 @@ const float _d = 1.2;
 
 /* macros
  * ENV_SCENE_?  : environment obstacle scenario
- * DEBUG_PRINT  : enable/disable debug print
 */
 #define ENV_SCENE_0     1
-#define DEBUG_PRINT     1
 
 #endif /* UTILS_CONSTANTS_H
 */
